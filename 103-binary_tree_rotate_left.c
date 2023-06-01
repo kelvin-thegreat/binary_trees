55875f03e4e2d740d962ca9a0ca1bd535bc8e1a0
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-06-01 13:18:14 -0700

    Rotate left

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_rotate_left - Left-rotates a binary tree.
- * @tree: A pointer to the root node of the tree to rotate.
+ * @tree: ...
  *
- * Return: A pointer to the new root node after rotation.
+ * Return: pointer to the new root node after rotation.
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
