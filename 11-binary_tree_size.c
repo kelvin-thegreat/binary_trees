315842ee4da2556aac7cff52611e268eff63de19
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-06-01 05:12:34 -0700

    Size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the size of.
+ *
+ * Return: The size of the tree.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	return (tree ? 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right) : 0);
+}
+
