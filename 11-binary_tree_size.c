1f13f139bdc67dd4a9324d9afa257cd6a1188dea
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-06-01 05:16:14 -0700

    Size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,6 +8,9 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	return (tree ? 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right) : 0);
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
 
